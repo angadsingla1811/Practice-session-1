269542ef592e2041ef7b48791666bffcf8d460cc
angadsingla1811 <ankurs.tutor@gmail.com>
2025-12-11 10:19:25 +0700

    second

--- main4.py
+++ main4.py
@@ -11,16 +11,17 @@
 """
 
 print("Hello Sir/Madam. Today I am your movie manager.")
-price_mov_tick = float(input("How much is the price of one movie ticket:"))
+print("1 ticket costs 100 rupees")
 people_go = float(input("How many people are going:"))
-price_pop = float(input("What is the price of 1 popcorn:"))
+print("1 popcorn costs 50 rupees")
 num_pop = float(input("How many popcorns will you buy:"))
 if (people_go):
     Discount = True
     disc_value = 0.10
     GST_value = 0.18
     print("Because 4 or more than 4 people are going you get a 10% discount!")
-total = price_mov_tick * people_go + price_pop * num_pop
+total = 100 * people_go + 50 * num_pop
 total_with_discount = total - (disc_value * total)
 total_with_GST = total_with_discount + (GST_value * total_with_discount)
 print("So your bill adss up to:", total_with_GST)
+print("1 Refill available! Have fun and enjoy your movie!!")
